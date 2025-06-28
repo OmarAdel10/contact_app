8f550f49d5ecc83bde9da471b3221622202ad0eb
Omar Adel <3maradel010@gmail.com>
2025-06-28 23:17:34 +0300

    appBar implemenation

--- lib/homePage_screen.dart
+++ lib/homePage_screen.dart
@@ -10,7 +10,16 @@ class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        leadingWidth: MediaQuery.sizeOf(context).width *0.5,
+        leading: Image.asset('assets/images/appBar_image.png'),
+        backgroundColor: Color(0xFF29384D),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFF29384D),
+        ),
+      ),
     );
   }
 }
