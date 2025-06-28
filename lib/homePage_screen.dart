d00d27b22bb61354a117c7c035504f1e56713226
Omar Adel <3maradel010@gmail.com>
2025-06-29 01:28:32 +0300

    - Adding floating action button with add icon for contact creation - Implementing bottom sheet modal dialog interface - Adding Lottie animation package

--- lib/homePage_screen.dart
+++ lib/homePage_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePageScreen extends StatefulWidget {
@@ -11,25 +12,43 @@ class _HomePageScreenState extends State<HomePageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leadingWidth: MediaQuery.sizeOf(context).width *0.5,
+        leadingWidth: MediaQuery.sizeOf(context).width * 0.55,
         leading: Image.asset('assets/images/appBar_image.png'),
         backgroundColor: Color(0xFF29384D),
       ),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 18),
-        decoration: BoxDecoration(
-          color: Color(0xFF29384D),
-        ),
+        decoration: BoxDecoration(color: Color(0xFF29384D)),
         child: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset('assets/images/homepage_middle_image.png'),
-              Text('There is No Contacts Added Here',style: TextStyle(color: Color(0xFFFFF1D4), fontSize: 20, fontWeight: FontWeight.w500),),
+              Text(
+                'There is No Contacts Added Here',
+                style: TextStyle(
+                  color: Color(0xFFFFF1D4),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _bottomSheet(context);
+        },
+        backgroundColor: Color(0xFFFFF1D4),
+        elevation: 3,
+        child: Icon(CupertinoIcons.add),
+      ),
     );
   }
+  
+  void _bottomSheet(BuildContext context) {
+    showModalBottomSheet(context: context, builder: (_) => Container());
+  }
+
 }
