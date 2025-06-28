4a1517b3ce71547b880b789d2ad1332e0aee4fb4
Omar Adel <3maradel010@gmail.com>
2025-06-28 23:00:38 +0300

    home screen initialize

---
+++ lib/homePage_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatefulWidget {
+  static const String routeName = '/home';
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
